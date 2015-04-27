b03ec6e9336f23d321eb05fb4fce4e96035b9853
Thomas <thomas.andreo@gmail.com>
2015-04-27 18:57:46 -0400

    Refactor + reindented templates

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,19 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>A'pisserie</title>
+        <title>Apisserie</title>
 
-    <link href="{{ url('/') }}/css/style.css" rel="stylesheet">
-  </head>
-  <body>
-		
-		@yield('content')
+        <link href='http://fonts.googleapis.com/css?family=Boogaloo' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <link href="{{ url('/') }}/css/style.css" rel="stylesheet">
+    </head>
+    <body>
 
-	  <script src="{{ url('/') }}/js/jquery.2.1.1.min.js"></script>
-    <script src="{{ url('/') }}/js/scripts.js"></script>
+        <header id="main-header">
 
-  </body>
+            <h1>Apisserie</h1>
+
+            <ul>
+                <li class="print">
+                    <a href="/print">Print</a>
+                </li>
+                <li class="clear-all">
+                    <span>Clear items</span>
+                </li>
+
+                @if ( Auth::guest() )
+
+                    <li>
+                        <a href="auth/login">Login</a>
+                    </li>
+
+                @endif
+
+            </ul>
+
+            @if ( Auth::check() )
+
+                Logged in as {{ Auth::user()->name }}
+
+            @endif
+
+        </header>
+
+        <main>
+
+            @yield('content')
+
+        </main>
+
+        <script src="{{ url('/') }}/js/jquery.2.1.1.min.js"></script>
+        <script src="{{ url('/') }}/js/scripts.js"></script>
+
+    </body>
 </html>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,76 +2,52 @@
 
 @section('content')
 
-<div id="screen" class="admin">
+@if ( Auth::check() )
 
-    <header id="main-header">
-        <h1>A'pisserie</h1>
-        <ul>
-            <li class="print">
-                <span>Print</span>
+    {!! Form::open([ 'url' => 'products/create' ]) !!}
+
+        {!! Form::text( 'name' ) !!}
+        {!! Form::select( 'section_id', $indexedSections ) !!}
+        {!! Form::submit() !!}
+
+    {!! Form::close() !!}
+
+@endif
+
+@if ( $errors->any() )
+
+    <div class="ui-notification error">
+
+        @foreach ( $errors->all() as $error )
+
+            {{ $error }}
+
+        @endforeach
+
+    </div>
+
+@endif
+
+<section class="section">
+
+    <ul>
+
+        @foreach ( $products as $product )
+
+            <li class="product" data-pid="{{ $product->id }}">
+                <div>
+                    <span class="name">
+                        {{ $product->name }}
+                    </span>
+                    <input type="text">
+                    <div class="remove"></div>
+                </div>
             </li>
-            <li class="clear-all">
-                <span>Clear items</span>
-            </li>
 
-            @if ( Auth::guest() )
+        @endforeach
 
-                <li>
-                    <a href="auth/login">Login</a>
-                </li>
+    </ul>
 
-            @endif
-
-        </ul>
-
-        @if ( Auth::check() )
-
-            Logged in as {{ Auth::user()->name }}
-
-        @endif
-
-    </header>
-
-    <main>
-
-        @if ( $errors->any() )
-
-            <div class="ui-notification error">
-
-                @foreach ( $errors->all() as $error )
-
-                    {{ $error }}
-
-                @endforeach
-
-            </div>
-
-        @endif
-
-        <section class="section">
-
-            <ul>
-
-                @foreach ( $products as $product )
-
-                    <li class="product" data-pid="{{ $product->id }}">
-                        <div>
-                            <span class="name">
-                                {{ $product->name }}
-                            </span>
-                            <input type="text">
-                            <div class="remove"></div>
-                        </div>
-                    </li>
-
-                @endforeach
-
-            </ul>
-
-        </section>
-
-    </main>
-
-</div>
+</section>
 
 @stop
