603ea6cbe0a49bd859b93c2553b881e1184aa7f3
Thomas <thomas.andreo@gmail.com>
2015-05-14 23:38:34 -0400

    Moved empty localStorage to separate class

---
+++ resources/assets/js/classes/Trash.js
@@ -0,0 +1,57 @@
+var Trash = function ()
+{
+    var $this = this;
+
+    // items to remove from localStorage
+    this.items = [];
+
+    this.fill = function ()
+    {
+        if ( localStorage.length )
+        {
+            // iterate through saved items
+            for ( var i = 0 ; i < localStorage.length ; i++ )
+            {
+                var pid = parseInt(localStorage.key(i));
+
+                if ( !isNaN(pid) )
+                {
+                    if ( localStorage[pid] === 'true' )
+                    {
+                        // prevent crash if a deleted product is still in the bag
+                        if ( !( pid in window.PRODUCTS ) )
+                        {
+                            $this.items.push(pid);
+                            continue;
+                        }
+
+                        // pre-select item
+                        window.PRODUCTS[pid].take();
+                    }
+                    else
+                    {
+                        // add to trash list
+                        $this.items.push(pid);
+                    }
+                }
+            }
+        }
+    }
+
+    // clean localStorage
+    this.empty = function ()
+    {
+        if ( !$this.items.length )
+        {
+            $this.fill();
+        }
+
+        if ( $this.items.length )
+        {
+            for ( var i = 0 ; i < $this.items.length ; i++ )
+            {
+                localStorage.removeItem($this.items[i]);
+            }
+        }
+    }
+}
--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -44,51 +44,10 @@ $(function()
     })
 
 
-
-
     /* onLoad Storage Management
     ----------------------------------------------- */
 
-    if ( localStorage.length )
-    {
-        // prep list of items to remove from localStorage
-        var trash = [];
-
-        // iterate through saved items
-        for ( var i = 0 ; i < localStorage.length ; i++ )
-        {
-            var pid = parseInt(localStorage.key(i));
-
-            if ( !isNaN(pid) )
-            {
-                if ( localStorage[pid] === 'true' )
-                {
-                    // prevent crash if a deleted product is still in the bag
-                    if ( !( pid in window.PRODUCTS ) )
-                    {
-                        trash.push(pid);
-                        continue;
-                    }
-
-                    // pre-select item
-                    window.PRODUCTS[pid].take();
-                }
-                else
-                {
-                    // add to trash list
-                    trash.push(pid);
-                }
-            }
-        }
-
-        // cleaning localStorage
-        if ( trash.length )
-        {
-            for ( var i = 0 ; i < trash.length ; i++ )
-            {
-                localStorage.removeItem(trash[i]);
-            }
-        }
-    }
+    var trash = new Trash();
+    trash.empty();
 
 })
