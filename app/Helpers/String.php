11ebc69e349bc1c0382d3b6344de522f06b675d6
Thomas Andreo <thomas.andreo@gmail.com>
2015-06-21 18:17:16 -0400

    Improved slugify for accented characters

--- app/Helpers/String.php
+++ app/Helpers/String.php
@@ -2,9 +2,20 @@
 
 class String
 {
+    private static $charMap =
+    [
+      'e' => ['é','è','ê','ë','É','È','Ê','Ë'],
+      'a' => ['à','â','À','Â'],
+      'c' => ['ç']
+    ];
 
     public static function slugify ( $string )
     {
+      foreach ( self::$charMap as $normalized => $charSet )
+      {
+        $string = str_replace( $charSet, $normalized, $string );
+      }
+
       // replace non letter or digits by -
       $string = preg_replace('~[^\\pL\d]+~u', '-', $string);
 
